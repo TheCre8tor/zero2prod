149fe0d4f9412815b14e194017226a3bfdd9b09a
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-20 17:09:17 +0100

    Got an indepth explanation on tokio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web::{get}, App, HttpRequest, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -10,10 +10,12 @@ async fn main() -> std::io::Result<()> {
     //! HttpServer, handles all transport level concerns.
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/", get().to(greet))
+            .route("/{name}", get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
     .await
 }
+
+
