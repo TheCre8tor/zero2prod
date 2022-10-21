a897efebfcc255e195ab497d02afe66f6a07c554
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-21 11:05:00 +0100

    Choosed A Random Port

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn health_check(_red: HttpRequest) -> impl Responder {
@@ -7,10 +9,10 @@ async fn health_check(_red: HttpRequest) -> impl Responder {
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     //! HttpServer, handles all transport level concerns.
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
 
     Ok(server)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,11 @@
 //! tests/health_check.rs
 
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange ->
-    spawn_app();
+    let address = spawn_app();
 
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
@@ -11,7 +13,7 @@ async fn health_check_works() {
 
     // Act ->
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -22,10 +24,17 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length())
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
