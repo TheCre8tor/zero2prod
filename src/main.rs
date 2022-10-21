40f8edb62ea1ae28919b44674451ac24e27ce40e
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-21 11:34:58 +0100

    Capturing Our Requirements As Tests

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@
 
 use std::net::TcpListener;
 
-use zero2prod::run;
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000")?;
 
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run(listener)?.await
+    zero2prod::run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,28 +2,6 @@
 
 use std::net::TcpListener;
 
-#[tokio::test]
-async fn health_check_works() {
-    // Arrange ->
-    let address = spawn_app();
-
-    // We need to bring in `reqwest`
-    // to perform HTTP requests against our application.
-    let client = reqwest::Client::new();
-
-    // Act ->
-    let response = client
-        .get(&format!("{}/health_check", &address))
-        .send()
-        .await
-        .expect("Failed to execute request");
-
-    // Assert ->
-    println!("{:?}", response.content_length());
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length())
-}
-
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
@@ -37,4 +15,46 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange ->
+    let app_address = spawn_app();
+
+    // We need to bring in `reqwest`
+    // to perform HTTP requests against our application.
+    let client = reqwest::Client::new();
+
+    // Act ->
+    let response = client
+        .get(&format!("{}/health_check", &app_address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert ->
+    println!("{:?}", response.content_length());
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length())
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange ->
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act ->
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert ->
+    assert_eq!(200, response.status().as_u16())
 }
