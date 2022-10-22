6941b1770c966b1e736a87c856f43a33274acd6d
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-22 14:09:12 +0100

    Capturing Our Requirements As Tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,7 @@ async fn health_check(_red: HttpRequest) -> impl Responder {
 // without having to use any proc-macro incantation.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     //! HttpServer, handles all transport level concerns.
-    let server = HttpServer::new(|| 
-        App::new()
-        .route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .listen(listener)?
         .run();
 
