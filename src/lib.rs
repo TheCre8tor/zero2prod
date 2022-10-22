b9bfb13577b3bd156e8644d7fb0b20ce0bdd2524
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-22 14:20:49 +0100

    Parsing Form Data From A POST Request

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,22 @@ async fn health_check(_red: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     //! HttpServer, handles all transport level concerns.
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
