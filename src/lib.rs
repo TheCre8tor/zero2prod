a7ff87a2dddc8999c1cbbc13bfce5be2a3dd2ca9
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-23 16:50:20 +0100

    Setting Configuration Management

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,3 @@
-use std::net::TcpListener;
-
-use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn health_check(_red: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-// We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async
-// without having to use any proc-macro incantation.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    //! HttpServer, handles all transport level concerns.
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 
 use std::net::TcpListener;
 
+use zero2prod::startup;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000")?;
 
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    zero2prod::run(listener)?.await
+    startup::run(listener)?.await
 }
