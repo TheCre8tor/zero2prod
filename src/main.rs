6ea0c90bb61bfdb82f4dbeeee8da9a1ba78daae6
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-23 17:27:22 +0100

    Reading A Configuration File

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 //! main.rs
 
 use std::net::TcpListener;
-
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
 
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
