7c6e5375687bba49c1afcf394b519be4acfd3954
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-23 18:40:59 +0100

    3.9.1 - Application State In actix-web

--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,20 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgConnection;
 
 use crate::routes::{health_check, subscribe};
 
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
     //! HttpServer, handles all transport level concerns.
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection)
     })
     .listen(listener)?
     .run();
