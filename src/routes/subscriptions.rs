bca9a83702a28ddbb2ae0291bcf359ec2feee8ec
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-25 09:06:26 +0100

    4.5.3 - tracing’s Span

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,8 +12,27 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
 
-    log::info!("[request_id: {}] - Adding '{}' '{}' as a new subscriber.", request_id, form.email, form.name);
-    log::info!("[request_id: {}] - Saving new subscriber details in the database", request_id);
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!(
+        "[request_id: {}] - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    tracing::info!(
+        "[request_id: {}] - Saving new subscriber details in the database",
+        request_id
+    );
     let query_result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -31,11 +50,18 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
 
     match query_result {
         Ok(_) => {
-            log::info!("[request_id: {}] - New subscriber details have been saved", request_id);
+            tracing::info!(
+                "[request_id: {}] - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
-        },
+        }
         Err(e) => {
-            log::error!("[request_id: {}] - Failed to execute query: {:?}", request_id, e);
+            tracing::error!(
+                "[request_id: {}] - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
