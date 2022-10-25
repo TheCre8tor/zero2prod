4d9df0806a159f865c79883be88509c2e715a960
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-25 08:35:19 +0100

    4.4.3 - Logs Must Be Easy To Correlate

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,8 +10,10 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!("Adding '{}' '{}' as a new subscriber.", form.email, form.name);
-    log::info!("Saving new subscriber details in the database");
+    let request_id = Uuid::new_v4();
+
+    log::info!("[request_id: {}] - Adding '{}' '{}' as a new subscriber.", request_id, form.email, form.name);
+    log::info!("[request_id: {}] - Saving new subscriber details in the database", request_id);
     let query_result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -29,11 +31,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
 
     match query_result {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            log::info!("[request_id: {}] - New subscriber details have been saved", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!("[request_id: {}] - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
