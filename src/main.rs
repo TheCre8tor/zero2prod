2b7cbbc6fd7a5ad8586b9e4a122dff7f131bb3b1
TheCre8tor <cre8tor.alexander@gmail.com>
2022-10-26 09:55:06 +0100

    4.5.10 - Cleaning Up Initialisation

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,24 @@
 
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
+    // Telemetry Setup & Initialization ->
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
-
-    // Panic if we can't read configuration
+    // Database configuration ->
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    // Port & Address setup ->
     let address = format!("127.0.0.1:{}", configuration.application_port);
-
     let listener = TcpListener::bind(address)?;
 
     // Bubble up the io::Error if we failed to bind the address
