43b30fde31364ff5a6fdf2ce4667559acd8796dc
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-12-28 14:18:03 +0100

    7.3.8.1 Extracting Our Startup Code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 //! main.rs
 
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
 use zero2prod::startup;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -14,39 +11,12 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    // Database configuration ->
+    // Environment Settings ->
     let configuration = get_configuration().expect("Failed to read configuration");
-    // let connection_pool =
-    //     PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-    //         .expect("Failed to connect to Postgres.");
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.db_with_name());
 
-    // Build a new email client
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
+    // Server Initialization ->
+    let server = startup::build(configuration).await?;
+    server.await?;
 
-    let timeout = configuration.email_client.timeout();
-
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    // Port & Address setup ->
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-
-    let listener = TcpListener::bind(address)?;
-
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    startup::run(listener, connection_pool, email_client)?.await
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,10 @@
+use crate::configuration::Settings;
 use crate::{
     email_client::EmailClient,
     routes::{health_check, subscribe},
 };
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
@@ -33,4 +35,38 @@ pub fn run(
     .run();
 
     Ok(server)
+}
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    // Database configuration ->
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.db_with_name());
+
+    // Build a new email client
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    // Port & Address setup ->
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+
+    let listener = TcpListener::bind(address)?;
+
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run(listener, connection_pool, email_client)
 }
