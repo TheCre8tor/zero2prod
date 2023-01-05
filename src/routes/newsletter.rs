6373035510c6846e91216a314cee91247b9df7d3
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-01-05 11:58:38 +0100

    9.5.1 Test Invalid Inputs

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,6 +1,18 @@
-use actix_web::{post, HttpResponse};
+use actix_web::{post, web, HttpResponse};
+
+#[derive(serde::Deserialize, Debug)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Content {
+    html: String,
+    text: String,
+}
 
 #[post("/newsletters")]
-pub async fn publish_newsletter() -> HttpResponse {
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
