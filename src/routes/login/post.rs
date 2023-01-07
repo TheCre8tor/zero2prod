bc46b4c27cbfd35c9c0809fda197d6b914322982
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-01-07 12:10:33 +0100

    10.6.3 Processing Form Data

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,8 +1,20 @@
 //! src/routes/login/login_api.rs
 
-use actix_web::{post, HttpResponse};
+use actix_web::http::header::LOCATION;
+use actix_web::{post, web, HttpResponse};
+use secrecy::Secret;
+
+#[derive(serde::Deserialize, Debug)]
+pub struct FormData {
+    username: String,
+    password: Secret<String>,
+}
 
 #[post("/login")]
-pub async fn login() -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn login(form: web::Form<FormData>) -> HttpResponse {
+    dbg!("{:?}", form);
+
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish()
 }
