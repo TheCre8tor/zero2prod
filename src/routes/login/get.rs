d4fcb82056905e4f97d1ed0396617e763922533b
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-01-08 06:13:49 +0100

    10.6.4.5 Add An HMAC Tag To Protect Query Parameters

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -2,20 +2,39 @@
 
 use actix_web::http::header::ContentType;
 use actix_web::{get, web, HttpResponse};
+use hmac::{Hmac, Mac};
+use secrecy::ExposeSecret;
 use tera::{escape_html, Tera};
+
+use crate::startup::HmacSecret;
 
 #[derive(serde::Deserialize)]
 pub struct QueryParams {
-    error: Option<String>,
+    error: String,
+    tag: String,
 }
 
+#[tracing::instrument(name = "Rendering Login Form", skip(query, secret))]
 #[get("/login")]
-pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
+pub async fn login_form(
+    query: Option<web::Query<QueryParams>>,
+    secret: web::Data<HmacSecret>,
+) -> HttpResponse {
     let read_file = include_str!("login.html");
 
-    let error_html = match query.0.error {
-        Some(message) => escape_html(&message),
+    let error_html = match query {
         None => "".into(),
+        Some(query_params) => match query_params.0.verify(&secret) {
+            Ok(error_msg) => escape_html(&error_msg),
+            Err(error) => {
+                tracing::warn!(
+                    error.message = %error,
+                    error.cause_chain = ?error,
+                    "Failed to verify query parameters using the HMAC tag"
+                );
+                "".into()
+            }
+        },
     };
 
     let mut tera = Tera::default();
@@ -32,4 +51,20 @@ pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(tera)
+}
+
+impl QueryParams {
+    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag)?;
+
+        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
+
+        let secret = secret.0.expose_secret().as_bytes();
+
+        let mut mac = Hmac::<sha2::Sha256>::new_from_slice(secret).unwrap();
+        mac.update(query_string.as_bytes());
+        mac.verify_slice(&tag)?;
+
+        Ok(self.error)
+    }
 }
