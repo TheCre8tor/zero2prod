56561e1f841b62aba1dbbc1411cab66593ec11fc
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-01-08 10:11:42 +0100

    10.6.4.10 How To Set A Cookie In actix-web

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,6 @@
 //! src/routes/login/login_api.rs
 
+use actix_web::cookie::Cookie;
 use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::{post, web, HttpResponse};
@@ -46,6 +47,7 @@ pub async fn login(
 
             let response = HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/login"))
+                .cookie(Cookie::new("_flash", error.to_string()))
                 .finish();
 
             Err(InternalError::from_response(error, response))
