79099b71d152fd129211d97427cbad68485fec50
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-09-12 06:06:51 +0100

    successfully run a database migation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //! src/lib.rs
 
 use actix_web::dev::Server;
-use actix_web::{web::get, App, HttpResponse, HttpServer};
+use actix_web::web::{get, post, Form};
+use actix_web::{App, HttpResponse, HttpServer};
 use std::net::TcpListener; // Transmission Control Protocol: [TCP]
 
 // NOTE: HTTP & TCP is a protocol
@@ -10,10 +11,24 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health-check", get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health-check", get().to(health_check))
+            .route("/subscriptions", post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
