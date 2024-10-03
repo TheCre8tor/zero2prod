13750c180bc90191cf62da186a44f886b551b329
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-10-03 18:55:55 +0100

    completed the email validation

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -29,6 +29,26 @@ mod tests {
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
 
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
+
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
@@ -45,11 +65,5 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
-    }
-
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        claims::assert_ok!(SubscriberEmail::parse(email));
     }
 }
